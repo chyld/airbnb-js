46d9ed4a77e7f14621311b5308e770175a2a65eb
Chyld Medford <chyld.medford@gmail.com>
2014-03-12 12:15:18 -0500

    register user function

--- test/unit/user_spec.js
+++ test/unit/user_spec.js
@@ -4,7 +4,9 @@
 
 process.env.DBNAME = 'airbnb-test';
 var expect = require('chai').expect;
+var Mongo = require('mongodb');
 var User;
+var bob;
 
 describe('User', function(){
 
@@ -18,7 +20,10 @@ describe('User', function(){
 
   beforeEach(function(done){
     global.nss.db.dropDatabase(function(err, result){
-      done();
+      bob = new User({role:'host', email:'bob@nomail.com', password:'1234'});
+      bob.register(function(){
+        done();
+      });
     });
   });
 
@@ -29,6 +34,27 @@ describe('User', function(){
       expect(u1.email).to.equal('bob@nomail.com');
       expect(u1.password).to.equal('1234');
       expect(u1.role).to.equal('host');
+    });
+  });
+
+  describe('#register', function(){
+    it('should register a new User', function(done){
+      var u1 = new User({role:'guest', email:'airbnb-guest@outlook.com', password:'1234'});
+      u1.register(function(err, body){
+        expect(err).to.not.be.ok;
+        expect(u1.password).to.have.length(60);
+        expect(u1._id).to.be.instanceof(Mongo.ObjectID);
+        body = JSON.parse(body);
+        expect(body.id).to.be.ok;
+        done();
+      });
+    });
+    it('should not register a new User', function(done){
+      var u1 = new User({role:'guest', email:'bob@nomail.com', password:'1234'});
+      u1.register(function(err){
+        expect(u1._id).to.be.undefined;
+        done();
+      });
     });
   });
 
